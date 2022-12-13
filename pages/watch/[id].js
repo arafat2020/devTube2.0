d4d638c6fa54f3a60283c7fafa83552e905d398e
Arafat <arafatmannan2019@gmail.com>
2022-12-13 08:15:27 +0600

    module bug

--- pages/watch/[id].js
+++ pages/watch/[id].js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Sajetion from "../../components/Sajetion";
-import Videoplayer from "../../components/videoplayer.jsx";
+import Videoplayer from "../../components/Videoplayer";
 
 function Watch() {
   const router = useRouter();
